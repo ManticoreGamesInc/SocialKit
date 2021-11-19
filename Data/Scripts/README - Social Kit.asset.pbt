03575601a0fcb2185953ed63fdda50c42477d6ab
coreslinkous <slinkous@manticoregames.com>
2021-11-19 12:01:52 -0500

    Start readme

---
+++ Data/Scripts/README - Social Kit.asset.pbt
@@ -0,0 +1,8 @@
+Assets {
+  Id: 5814858635094213307
+  Name: "README - Social Kit"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 101
+}
---
+++ Data/Scripts/README - Social Kit.lua
@@ -0,0 +1,51 @@
+--[[
+
+SOCIAL KIT
+
+
+	Summary
+
+
+TABLE OF CONTENTS
+-----------------
+
+
+INTERACTABLE FURNITURE
+----------------------
+
+
+CONSUMABLES
+-----------
+
+	Consumables are objects that players can buy in a shop, and use with an animation.
+
+Creating a New Consumable:
+
+	1. Open the "Definitions" folder, and then the "Consumables" folder
+	2. In the folder, there are three example definitions of Consumables: "Snow Cone", "Coffee", and "Burger".
+	3. In Project Content, in My Scripts, find "Consumable Definition" and drag it into the Consumables folder.
+	4. Fill out the Properties section to match your consumable
+
+Consumable Properties
+	
+	- id : Unique identifier for the consumable item. Does not have to match the displayName
+	- displayName : This will be visible in all the shops that sell the item, and in the confirmation dialogue to buy it
+	- description : What the item is. Shows up in the confirmation dialogue to buy it.
+	- category : 
+
+
+SHOPS
+-----
+
+	- 2 types of shops: single item, and a ... menu?
+
+Creating a New Shop:
+
+
+CURRENCY
+--------
+
+
+
+
+]]
